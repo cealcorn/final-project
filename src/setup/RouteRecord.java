204ddc0a53f83b96f32f32d33a0f4d55f596c445
cealcorn <cassidyealcorn@gmail.com>
2023-03-31 14:18:50 -0400

    Initial commit

---
+++ src/setup/RouteRecord.java
@@ -0,0 +1,10 @@
+package setup;
+
+import java.io.Serializable;
+
+
+public class RouteRecord implements Serializable {
+    //TODO: This class may be implemented as a "helper class" for helping implement the Table class
+    // This class is optional: if you do not need this class, you can keep it empty
+    
+}
