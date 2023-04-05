18653eb669ae9991bb8c85ecdbe1484e697b8490
Haley Blanton <h.blanton57@gmail.com>
2023-04-05 12:24:49 -0400

    Add data structure to Table

--- src/setup/Table.java
+++ src/setup/Table.java
@@ -1,11 +1,14 @@
 package setup;
 
 import java.io.Serializable;
+import java.util.Map;
 
 
 //This class represents the distance vector that a router initiates, optimizes, and exchanges with neighbors
 public class Table implements Serializable {
  //TODO: add any member variables and member methods.
+    //Represents <Starting point router, Map<Destination router, cost to travel>
+    private Map<Integer, Map<Integer, Integer>> distanceVector;
 
     @Override
     public String toString() {
